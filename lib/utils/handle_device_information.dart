31e69acc7e66fc06867319906777c3e6eba27e5b
Hasan Ugur <hasan.ugur@segmentify.com>
2024-01-18 16:52:05 +0300

    fix: fixed device os information

--- lib/utils/handle_device_information.dart
+++ lib/utils/handle_device_information.dart
@@ -21,7 +21,7 @@ import 'package:device_info_plus/device_info_plus.dart';
 Future<Map<String, String>> getDeviceInformation() async {
   DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
   String deviceName;
-  String deviceOS = Platform.operatingSystem.toLowerCase();
+  String deviceOS = Platform.operatingSystem.toUpperCase();
   String deviceOSVersion;
 
   if (Platform.isAndroid) {
